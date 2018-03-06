caae8e711e791d5cc81e00710d908610664e9b56
markilov <i.n.markilov@abv.bg>
2018-03-06 20:46:18 +0200

    First step to Blackjack app

---
+++ GettingStarted/BlackJakc/BlackJack.js
@@ -0,0 +1,12 @@
+//
+// Blackjack by Markilov
+//
+
+let card1 = "Ace of Spades";
+let card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack!");
+console.log("You are dealt: ");
+console.log("  " + card1);
+console.log("  " + card2);
+
